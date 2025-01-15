24029db26a355e5859aae7ba430536fcd40e2406
Ronaldo <yaperdole26@gmail.com>
2025-01-15 00:14:54 -0600

    fixed some bugs

--- store/users/views.py
+++ store/users/views.py
@@ -42,14 +42,14 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        form = ProfileForm(data=request.POST, isinstance=request.user,
+        form = ProfileForm(data=request.POST, instance=request.user,
                         files=request.FILES)
         if form.is_valid():
             form.save()
-            messages.succes(request, 'Profile was changed')
+            messages.success(request, 'Profile was changed')
             return HttpResponseRedirect(reverse('user:profile'))
     else:
-        form = ProfileForm(isinstance=request.user)
+        form = ProfileForm(instance=request.user)
         
         
     orders = Order.objects.filter(user=request.user).prefetch_related(
