e94e0ca67fe34b69fc0c1b255185f88fa7e04b51
Ronaldo <yaperdole26@gmail.com>
2025-01-14 21:42:27 -0600

    register and profile views created

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms 
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, \
+    UserCreationForm, UserChangeForm
 from .models import User
 
 
@@ -11,3 +12,40 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         models = User
         fields = ['username', 'password']
+        
+    
+class UserRegistrarionForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        )
+        first_name = forms.CharField()
+        last_name = forms.CharField()
+        username = forms.CharField()
+        email = forms.CharField()
+        passowrd1 = forms.CharField()
+        password2 = forms.CharField()
+        
+        
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User 
+        fields = (
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        )
+        
+        image = forms.ImageField(required = False)
+        first_name = forms.CharField()
+        last_name = forms.CharField()
+        username = forms.CharField()
+        email = forms.CharField()
--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.contrib import auth
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrarionForm, \
+    ProfileForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
+from orders.models import Order, OrderItem
 
 def login(request):
     if request.method == 'POST':
@@ -14,17 +18,50 @@ def login(request):
             
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:product'))
+                return HttpResponseRedirect(reverse('main:product_list'))
     else:
         form = UserLoginForm()
-        
     return render(request, 'users/login.html', {'form': form})
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegistrarionForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(
+                request, f'{user.username}, Succesful Registration!'
+            )
+            return HttpResponseRedirect(reverse('user:login'))
+    else:
+        form = UserRegistrarionForm()
     return render(request, 'users/register.html')
 
+
+@login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, isinstance=request.user,
+                        files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.succes(request, 'Profile was changed')
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(isinstance=request.user)
+        
+        
+    orders = Order.objects.filter(user=request.user).prefetch_related(
+        Prefetch(
+            'items',
+            queryset=OrderItem.objects.select_related('product'),
+        )
+    ).order_by('-id')
+    return render(request, 'users/profile.html',
+                {'form': form,
+                'orders': orders})
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:product_list'))
